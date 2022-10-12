452a12e040e663554c631177cb4f2563d475c985
Diego Sano <diego.sano@nave.rs>
2022-10-12 09:58:45 -0300

    feat: config miragejs fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from 'react'
+
 export function App() {
+  useEffect(() => {
+    fetch('/api/products')
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  }, [])
+
   return <div></div>
 }
